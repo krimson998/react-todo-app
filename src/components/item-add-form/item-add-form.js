31093bc94357ab34604f47bfbd0f24e25d1bcff1
krimson998 <orangefilin@gmail.com>
2020-07-14 14:54:45 +0300

    submit form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-  render() {
-    const { addNote } = this.props;
+  state = {
+    label: '',
+  };
 
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.addNote(this.state.label);
+  };
+
+  render() {
     return (
-      <div className="item-add-form">
-        <button
-          onClick={() => {
-            addNote('Some text');
-          }}
-          className="btn btn-outline-secondary"
-        >
-          Add a note
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done"
+        />
+        <button className="btn btn-outline-secondary">Add a note</button>
+      </form>
     );
   }
 }
